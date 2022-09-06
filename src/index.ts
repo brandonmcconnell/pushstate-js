133baa00db10f8f31d8a262a50a073bbab23d925
Brandon McConnell <brandon@dreamthinkbuild.com>
2022-09-06 18:34:20 -0400

    Switch AND to OR, update version

--- src/index.ts
+++ src/index.ts
@@ -9,9 +9,10 @@ const intiailizePushstateEvent = (debounceDelay = 1000) => {
   let lastRun = -1 * (debounceDelay + 1);
   let currentUrl = getCurrentUrl();
   const observer = new MutationObserver(() => {
-    let currentRun = performance.now();
-    let newUrl = getCurrentUrl();
-    if (newUrl !== currentUrl && currentRun - lastRun > debounceDelay) {
+    const currentRun = performance.now();
+    const timeSinceLastRun = currentRun - lastRun;
+    const newUrl = getCurrentUrl();
+    if (timeSinceLastRun > debounceDelay || newUrl !== currentUrl) {
       const previousUrl = currentUrl;
       lastRun = currentRun;
       currentUrl = newUrl;
